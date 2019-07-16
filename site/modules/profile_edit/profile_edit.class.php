57e57b6eff93ec78e7f2b903b99b011aad26c39f
SlavaChip <sciii@rambler.ru>
2019-07-16 17:21:57 +0300

    Все данные сохраняются правильно.

--- site/modules/profile_edit/profile_edit.class.php
+++ site/modules/profile_edit/profile_edit.class.php
@@ -3,21 +3,6 @@ class profile_edit extends aModule{
     function execute($arr)
     {
 
-/*
- [id] => 2
-    [login] => Igor
-    [password] => 123
-    [name] => Василий
-    [surname] => ПЕтров
-    [city] => Владивосток
-    [birthday] => 0000-00-00
-    [phone] => +8 (333) 444-55-66
-    [create_time] => 2019-07-01 09:00:00
-    [rating] => 0.9
-    [money] => 34567.0
-    [gender] => 1
-    [email] => test@rambler.ru
-)* */
       if (isset($_REQUEST['scSendBtn'])) {
         $cName = $_REQUEST['name'];
         $cSurname = $_REQUEST['last_name'];
@@ -27,7 +12,11 @@ class profile_edit extends aModule{
           $cGender = 1;
         }
         $cCity = $_REQUEST['city'];
-        $cBirthday = $_REQUEST['date_Birt'];
+        $cDate = $_REQUEST['date_Birt'];
+        $cDay  = mb_substr($cDate, 0,2);
+        $cMonth = mb_substr($cDate, 3,2);
+        $cYear = mb_substr($cDate, 6,4);
+        $cBirthday = $cYear . '.' . $cMonth . '.' . $cDay;
         $cEmail = $_REQUEST['email'];
         $cPhone = $_REQUEST['phone'];
         $cQueue = "UPDATE users SET name='{$cName}', surname='{$cSurname}', gender={$cGender}, city='{$cCity}',"
@@ -47,9 +36,6 @@ class profile_edit extends aModule{
         $_SESSION['user'][gender]   = $cGender;
         $_SESSION['user'][email]    = $cEmail;
 
-echo '<pre>';
-print_r ($cQueue);
-echo '</pre>';
       }
         //echo "<pre>"; print_r($arr); echo "</pre>"; //die();   
 		$in = $arr["send_params"];
@@ -76,12 +62,6 @@ echo '</pre>';
       $_SESSION['smarty']->assign('email', $email[0]['v']);
       $_SESSION['smarty']->assign('phone', $phone[0]['v']);
       $_SESSION['smarty']->assign('birthday', $birthday[0]['v']);
-
-echo '<pre>';
-print_r ($_REQUEST);
-echo '</pre>';
-
-//    $selInf = rows('SELECT count(id_client) FROM tasks WHERE id_client='.$_SESSION['contractor']['id']);
 
 		$_SESSION['smarty']->display('profile_edit/profile_edit.tpl');
         
