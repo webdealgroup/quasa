3a99f31c25f2804d4b576c9162ad46011b246f00
SlavaChip <sciii@rambler.ru>
2019-07-16 12:42:39 +0300

    Исправлен первый запрос, убрано лишнее

--- site/modules/user_card/user_card.class.php
+++ site/modules/user_card/user_card.class.php
@@ -11,15 +11,7 @@ class user_card extends aModule{
     $_SESSION['smarty']->assign('tasks', $tasks);
 
       $j = 0;
-      $tasks_no = rows("SELECT count(id_client) as v FROM tasks WHERE id_client=" . $_SESSION['smarty']->_tpl_vars['user']['id']);
-//      for ($i=0; $i<count($_SESSION['smarty']->_tpl_vars['tasks']); $i++) {
-//        if ($_SESSION['smarty']->_tpl_vars['user']['id'] == $_SESSION['smarty']->_tpl_vars['tasks'][$i]['id_client']) { $j++; }
-//      }
-//echo '<pre>';
-//print_r ($tasks_no);
-//echo $tasks_no[0]['v'] . '<br/>';
-//echo '</pre>';
-
+      $tasks_no = rows("SELECT count(id_client) as v FROM tasks WHERE id_client=" . $_SESSION['user']['id']);
       $_SESSION['smarty']->assign('task_no', $tasks_no[0]['v']);
 
 	  	$_SESSION['smarty']->display('user_card/user_card.tpl');
