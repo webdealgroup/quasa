869130b453d1f8d11ef8ea243b788970b92ca1c1
SlavaChip <sciii@rambler.ru>
2019-07-17 15:27:53 +0300

    Убрал лишнее

--- site/modules/pay/pay.class.php
+++ site/modules/pay/pay.class.php
@@ -7,18 +7,12 @@ class pay extends aModule{
 		    $in = $arr["send_params"];
 
 
-<<<<<<< HEAD
         $money = rows("SELECT money as v FROM users WHERE id=" . $_SESSION['user']['id']);
 
         $_SESSION['smarty']->assign('money', ceil($money[0]['v'])); // округление в большую сторону, floor() - в меньшую
 
         $_SESSION['smarty']->display('pay/pay.tpl');
 
-=======
-
-	  	  $_SESSION['smarty']->display('pay/pay.tpl');
-        
->>>>>>> 68f22ca496c692d9c8078f2987330dbbc25f858f
     }
 }
 ?>
