adad4e73e995c99ca047de15b82916e1da5c2069
SlavaChip <sciii@rambler.ru>
2019-07-19 08:19:25 +0300

    Убрал тестовый вывод

--- site/modules/create_task/create_task.class.php
+++ site/modules/create_task/create_task.class.php
@@ -5,29 +5,29 @@ class create_task extends aModule{
         //echo "<pre>"; print_r($arr); echo "</pre>"; //die();   
 		$in = $arr["send_params"];
       if (isset($_REQUEST['scSendBtn'])) {
-echo '<pre>';
-print_r ($_REQUEST);
-echo '</pre>';
+//echo '<pre>';
+//print_r ($_REQUEST);
+//echo '</pre>';
 $cUser = $_SESSION['user']['id'];
 $cDesc = $_REQUEST['name'];
 // insert into tasks (id_client, title, description) values (3, 'Тест', 'cDesc')
 // insert into tasks (id, id_client, id_contractor, title, description, address, cost, time_start, time_end, completed)
 $cQueue = "insert into tasks (id_client, title, description) '
         . 'values ({$cUser}, 'Тест', '{$cDesc}')";
-q($cQueue);
+//q($cQueue);
       }
 
     $cat_id = isset($_REQUEST['cat_id']) ? $_REQUEST['cat_id'] : 0;
     if ($cat_id > 2) {
       $cat_parent = row('SELECT id_parent FROM cat WHERE id = ' . $cat_id);
       $cat_id = $cat_parent['id_parent'];
-echo '<pre>id_parent=';
-print_r ($cat_id);
-echo '</pre>';
+//echo '<pre>id_parent=';
+//print_r ($cat_id);
+//echo '</pre>';
     }
-      echo '<pre>cat_id=';
-      print_r ($cat_id);
-      echo '</pre>';
+//      echo '<pre>cat_id=';
+//      print_r ($cat_id);
+//      echo '</pre>';
 
     $cat_items = rows('SELECT * FROM cat WHERE id_parent = ' . $cat_id);
 
