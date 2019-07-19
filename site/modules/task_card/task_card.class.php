10f2bc4f4e9907ac2caa1dbf64980f206da6a99c
Wizzard <52732416+Westercraft@users.noreply.github.com>
2019-07-19 16:33:15 +0300

    Update task_card.class.php

--- site/modules/task_card/task_card.class.php
+++ site/modules/task_card/task_card.class.php
@@ -1,16 +1,15 @@
 <?php include_once('site/modules/aModule.class.php');
 class task_card extends aModule{
     function execute($arr)
-    {           
-        //echo "<pre>"; print_r($arr); echo "</pre>"; //die();   
-		$in = $arr["send_params"];
-		
-		$tasks = rows('SELECT * FROM tasks WHERE id ='.$in['id']);
+    {
+        //echo "<pre>"; print_r($arr); echo "</pre>"; //die();
+        $in = $arr["send_params"];
 
-	    $_SESSION['smarty']->assign('referrer', $in['referrer']); 
-	    $_SESSION['smarty']->assign('tasks', $tasks); 
-		$_SESSION['smarty']->display('task_card/task_card.tpl');
-        
+        //$tasks = rows('SELECT * FROM tasks WHERE id ='.$in['id']);
+        $_SESSION['smarty']->assign('referrer', $in['referrer']);
+        $_SESSION['smarty']->assign('tasks', $tasks);
+        $_SESSION['smarty']->display('task_card/task_card.tpl');
+
     }
 }
 ?>
