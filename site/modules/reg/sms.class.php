77c4714084cd3a2e6db2460c236583ff0c61b7f0
Meowze3 <meowze3@icloud.com>
2019-07-27 19:50:18 +0300

    Create sms.class.php
    Add SMS error

---
+++ site/modules/reg/sms.class.php
@@ -0,0 +1,8 @@
+<?php include_once('site/modules/aModule.class.php');
+class sms extends aModule{
+    function execute($arr){           
+			echo "Временно недоступна отправка SMS";
+		}
+        
+    }
+?>
